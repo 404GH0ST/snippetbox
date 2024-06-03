48a39dddcbb30d153144e0f43f6f2ab275075ad3
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2024-06-03 22:26:55 +0700

    feat: separate routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -26,24 +26,11 @@ func main() {
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
-	mux := http.NewServeMux()
 
-	// Create a handler for serving static files at ./ui/static directory from the project root directory
-	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
-
-	// Register the fileServer to /static/ URL path, we must strip the /static from the URL
-	// If we didn't strip it, the fileServer handler path will look for a file in ./ui/static/static/ which doesn't exist
-	// Using the StripPrefix, the path that passed to fileServer handler will become ./ui/static/ which is a valid path
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /", app.home)
-	mux.HandleFunc("GET /snippet/view", app.snippetView)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreate)
-
-	srv := http.Server{
+	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s", *addr) // Information message
