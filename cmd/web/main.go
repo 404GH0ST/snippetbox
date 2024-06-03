62c3fc39321dff08ab1c1c252b2e7a90802a150e
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2024-06-03 16:20:29 +0700

    fix: adjust directory path

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,12 +8,12 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	// Create a handler for serving static files at ./ui/static/ directory from the project root directory
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	// Create a handler for serving static files at ./ui/static directory from the project root directory
+	fileServer := http.FileServer(http.Dir("./ui/static"))
 
 	// Register the fileServer to /static/ URL path, we must strip the /static from the URL
-	// If we didn't strip it, the fileServer handler path will look for a file in ./ui/static//static/ which doesn't exist
-	// Using the StripPrefix, the path that passed to fileServer handler will become ./ui/static// which is a valid path
+	// If we didn't strip it, the fileServer handler path will look for a file in ./ui/static/static/ which doesn't exist
+	// Using the StripPrefix, the path that passed to fileServer handler will become ./ui/static/ which is a valid path
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("GET /", home)
