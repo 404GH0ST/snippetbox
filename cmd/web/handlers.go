0f6a24cc39095db224d42d8861b588da4212140a
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2024-06-03 16:18:06 +0700

    doc: add comment

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,17 +9,20 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	// Make sure home handler can only be accessed from / path because of the subtree pattern that acts like catch-all
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
 
+	// Define required template file
 	files := []string{
 		"./ui/html/base.html",
 		"./ui/html/partials/nav.html",
 		"./ui/html/pages/home.html",
 	}
 
+	// Parse all the templates file, using variadic argument so that it reads all the slice element
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
@@ -27,6 +30,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Render the base template, because in base template it will invoke other template
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
@@ -35,7 +39,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
+	// Convert GET id parameter to an int
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	// Make sure it's a valid positive integer
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,9 +7,15 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	// Create a handler for serving static files at ./ui/static/ directory from the project root directory
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
+	// Register the fileServer to /static/ URL path, we must strip the /static from the URL
+	// If we didn't strip it, the fileServer handler path will look for a file in ./ui/static//static/ which doesn't exist
+	// Using the StripPrefix, the path that passed to fileServer handler will become ./ui/static// which is a valid path
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("GET /", home)
 	mux.HandleFunc("GET /snippet/view", snippetView)
 	mux.HandleFunc("POST /snippet/create", snippetCreate)
