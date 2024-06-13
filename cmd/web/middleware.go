b2496623094e086c388caf919c4537e96aabfb88
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2024-06-13 14:21:55 +0700

    feat: add unit and sub tests for humanDate and commonHeaders function

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -13,7 +13,7 @@ func commonHeaders(next http.Handler) http.Handler {
 		// Any code here will be execute on the way down the chain.
 		w.Header().
 			Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
-		w.Header().Set("Referer-Policy", "origin-when-cross-origin")
+		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "deny")
 		w.Header().Set("X-XSS-Protection", "0")
---
+++ cmd/web/middleware_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/404GH0ST/snippetbox/internal/assert"
+)
+
+func TestCommonHeaders(t *testing.T) {
+	rr := httptest.NewRecorder()
+
+	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a mock HTTP handler that we can pass to our commonHeaders
+	// middleware, which writes a 200 status code and an "OK" response body.
+	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	commonHeaders(next).ServeHTTP(rr, r)
+
+	rs := rr.Result()
+
+	expectedValue := "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com"
+	assert.Equal(t, rs.Header.Get("Content-Security-Policy"), expectedValue)
+
+	expectedValue = "origin-when-cross-origin"
+	assert.Equal(t, rs.Header.Get("Referrer-Policy"), expectedValue)
+
+	expectedValue = "nosniff"
+	assert.Equal(t, rs.Header.Get("X-Content-Type-Options"), expectedValue)
+
+	expectedValue = "deny"
+	assert.Equal(t, rs.Header.Get("X-Frame-Options"), expectedValue)
+
+	expectedValue = "0"
+	assert.Equal(t, rs.Header.Get("X-XSS-Protection"), expectedValue)
+
+	expectedValue = "Go"
+	assert.Equal(t, rs.Header.Get("Server"), expectedValue)
+
+	// Check that middleware has correctly called the next handler in line
+	// and the response status code and body are as expected
+
+	assert.Equal(t, rs.StatusCode, http.StatusOK)
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body = bytes.TrimSpace(body)
+	assert.Equal(t, string(body), "OK")
+}
